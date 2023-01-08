f3d5a4156798fddd972e6444f9617880c243d7cd
ranadipto <ranadipto@gmail.com>
2023-01-08 22:48:16 +0530

    admin dashboard

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import store from './store'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const options = {
+
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+   <AlertProvider template={AlertTemplate} {...options}>
     <App />
-  </React.StrictMode>
+    </AlertProvider>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
